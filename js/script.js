06a8490fc817661388a1dd3306cc3df86b76cc0e
Ivelina Ruseva <91214544+rusevaIvelina@users.noreply.github.com>
2021-10-12 19:32:15 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let pokemonlist = [
+  {name:'Corsola', height:'0.6', type: ['water', 'rock']},
+  {name:'Raichu', height:'0.8', type: ['electric']},
+  {name:'Seel', height:'1.1', type: ['water']},
+  {name:'Mamoswine', height:'2.5', type: ['ice', 'ground']},
+  {name:'Vileplume', height:'1.2', type: ['grass', 'poison']},
+  {name:'Ponyta', height:'1.0', type: ['fire']}
+]
