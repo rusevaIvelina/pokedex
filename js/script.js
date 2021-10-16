12097f39c0e422f3c8e6342fbd846692059843f9
rusevaIvelina <iva.ruseva14@gmail.com>
2021-10-16 13:09:00 -0400

    Update script.js
    making my code better to read

--- js/script.js
+++ js/script.js
@@ -3,37 +3,15 @@
 let pokemonRepository = (function() {
 
 let pokemonList = [
-  {
-   name:'Corsola',
-   height: 0.6,
-   type: ['water', 'rock']
-  },
-    {
-   name:'Raichu',
-   height: 0.8,
-   type: ['electric']
-   },
-    {
-    name:'Seel',
-    height: 1.1,
-    type: ['water']
-   },
-    {
-    name:'Mamoswine',
-     height: 2.5,
-      type: ['ice', 'ground']
-   },
-     {
-    name:'Vileplume',
-    height: 1.2,
-    type: ['grass', 'poison']
-   },
-     {
-    name:'Ponyta',
-    height: 1.0,
-    type: ['fire']
-    }
+  { name:'Corsola', height: 0.6, type: ['water', 'rock'] },
+  { name:'Raichu', height: 0.8, type: ['electric'] },
+  { name:'Seel', height: 1.1, type: ['water'] },
+  { name:'Mamoswine', height: 2.5, type: ['ice', 'ground'] },
+  { name:'Vileplume', height: 1.2, type: ['grass', 'poison'] },
+  { name:'Ponyta', height: 1.0, type: ['fire'] }
+
 ];
+
  // Adds a new pokemon at the end of the pokemonList with the correct type of data
 function add(pokemon) {
   if (typeof pokemon === 'object') {
@@ -53,4 +31,6 @@ return {
 };
 })();
 
-console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function(pokemon) {
+  console.log(pokemonRepository.getAll(pokemon));
+ });
