d744a002eaed8d76763bb7e4ee8889162f39cd54
rusevaIvelina <iva.ruseva14@gmail.com>
2021-10-16 13:16:36 -0400

    Update script.js
    Replaced  the typeof object with more detailed properties

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@
 let pokemonRepository = (function() {
 
 let pokemonList = [
-  { name:'Corsola', height: 0.6, type: ['water', 'rock'] },
-  { name:'Raichu', height: 0.8, type: ['electric'] },
-  { name:'Seel', height: 1.1, type: ['water'] },
-  { name:'Mamoswine', height: 2.5, type: ['ice', 'ground'] },
-  { name:'Vileplume', height: 1.2, type: ['grass', 'poison'] },
-  { name:'Ponyta', height: 1.0, type: ['fire'] }
+   { name:'Corsola', height: 0.6, type: ['water', 'rock'] },
+   { name:'Raichu', height: 0.8, type: ['electric'] },
+   { name:'Seel', height: 1.1, type: ['water'] },
+   { name:'Mamoswine', height: 2.5, type: ['ice', 'ground'] },
+   { name:'Vileplume', height: 1.2, type: ['grass', 'poison'] },
+   { name:'Ponyta', height: 1.0, type: ['fire'] }
 
 ];
 
  // Adds a new pokemon at the end of the pokemonList with the correct type of data
 function add(pokemon) {
-  if (typeof pokemon === 'object') {
+  if (typeof pokemon === 'name, height, type') {
   pokemonList.push(pokemon);
   } else {
     document.write('Not a pokemon')
