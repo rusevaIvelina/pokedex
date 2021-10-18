0fc187eb71a47f7bd61ca9d7c5e5a1829f8738c6
rusevaIvelina <iva.ruseva14@gmail.com>
2021-10-18 12:53:50 -0400

    Update script.js
    created a new function "addListItem"

--- js/script.js
+++ js/script.js
@@ -24,21 +24,25 @@ let pokemonRepository = (function() {
     function getAll() {
       return pokemonList;
     }
+
+    function addListItem(pokemon) {
+      let pokemonList = document.querySelector('.pokemon-list');
+      let listpokemon = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('button-class');
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+    }
     
     return {
       add:add,
-      getAll: getAll
+      getAll: getAll,
+      addListItem: addListItem
     };
     })();
     
     pokemonRepository.getAll().forEach(function(pokemon) {
-      let pokemonList = document.querySelector('.pokemon-list');
-      let listpokemon = document.createElement('li');
-      let button = document.createElement('button');
-      button.innerText = 'placeholder';
-      button.classList.add('button-class');
-      listpokemon.appendChild(button);
-      pokemonList.appendChild(listpokemon);
-      
+        pokemonRepository.addListItem(pokemon);
     });
    
